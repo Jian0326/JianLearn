f207d4eccd063b8644b05c4b201c7ec29c490c07
Jerry <jian820326@gmail.com>
2017-05-18 13:54:33 +0800

    fff
    111

---
+++ Assets/Scripts/Load/LoadScene.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    private AsyncOperation asyncOp;
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private Text progressText;
+    [SerializeField]
+    private string sceneName;
+    void Start()
+    {
+        if (GameConfig.SceneName != string.Empty)
+        {
+            sceneName = GameConfig.SceneName;
+        }
+        Debug.Log(sceneName);
+        StartCoroutine(StartLoad());
+    }
+
+    private IEnumerator StartLoad()
+    {
+        yield return new WaitForEndOfFrame();
+        asyncOp = SceneManager.LoadSceneAsync(sceneName);
+        asyncOp.allowSceneActivation = false;
+        int totalPar = 0;
+        int currentPar = 0;
+        while (asyncOp.progress < 0.9f)
+        {
+            totalPar = (int)(asyncOp.progress * 100);
+            while (currentPar < totalPar)
+            {
+                currentPar++;
+                SetProgress(currentPar);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        totalPar = 100;
+        while (currentPar < totalPar)
+        {
+            currentPar++;
+            SetProgress(currentPar);
+            yield return new WaitForEndOfFrame();
+        }
+        asyncOp.allowSceneActivation = true;
+        yield return asyncOp;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SetProgress(int pro)
+    {
+        progressText.text = string.Format("{0}%", pro);
+        slider.value = pro;
+    }
+}
---
+++ Assets/Scripts/Utils/LoadCsv.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    class LoadCsv
+    {
+        private string csvUrl = PlatformConfig.WwwURL + "/csv/";
+        public string[] StartLoadCsv(string csv)
+        {
+            WWW www = new WWW(csvUrl + csv);
+            string text;
+            while (!www.isDone)
+            {
+            }
+            text = www.text;
+            string[] res = text.Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+            return res;
+        }
+        public string[][] StartLoadCsvTwo(string csv)
+        {
+            string[] csvs = StartLoadCsv(csv);
+            int len = csvs.Length;
+            string[][] res = new string[len][];
+            string[] strs;
+            for (int i = 0;i<len;i++)
+            {
+                strs = csvs[i].Split(new string[] { ","}, System.StringSplitOptions.RemoveEmptyEntries);
+            }
+            return res;
+        }
+    }
+}
